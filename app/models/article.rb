6f1d4057b89f91756b73a775fbefd846978663d8
Garima <garimasharma4998@gmail.com>
2020-12-01 12:51:18 +0530

    adding one to many association of users and articles, temp assigning of all articles to first user_id

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
   validates :title, presence:true, length: {minimum:4 ,maximum:13}
   validates :description, presence:true, length: {minimum:7 ,maximum:400}
-
+  belongs_to :user
 
 end
